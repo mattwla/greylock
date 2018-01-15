645747a453dcf677444cc6e94e2f61aafc7cc5c0
mattwla <mattlarrabee@gmail.com>
2018-01-15 10:27:01 -0500

    cleans journey a bit, fixes another travel bug

--- apps/openmw/mwtasks/journey.cpp
+++ apps/openmw/mwtasks/journey.cpp
@@ -38,72 +38,65 @@
 namespace MWTasks
 {
 
-	Journey::Journey()
-	{
-	}
+	//Journey::Journey()
+	//{
+	//}
 
-	Journey::Journey(MWWorld::Ptr dest):
-		mDestination(dest)
-	{
-		
-		mStep = 0;
-		mStartTime = MWBase::Environment::get().getWorld()->getTimeStamp();
-		mDone = false;
-	}
+	//Journey::Journey(MWWorld::Ptr dest):
+	//	mDestination(dest)
+	//{
+	//	
+	//	mStep = 0;
+	//	//mStartTime = MWBase::Environment::get().getWorld()->getTimeStamp();
+	//	mDone = false;
+	//}
 
 	Journey::Journey(std::string destId, std::string npcId, float range):
 		mDestId(destId)
 		, mRange(range)
 		, mHeadedToDoor(false)
 	{
-		mWasActiveLastUpdate = MWBase::Environment::get().getTasksManager()->isInActiveRange(npcId);
+		mWasActiveLastUpdate = MWBase::Environment::get().getTasksManager()->isInActiveRange(npcId); //when task was created, was npc in active range of player?
 		mNpcId = npcId;
 		mStep = 0;
-		mStartTime = MWBase::Environment::get().getWorld()->getTimeStamp();
-		init();
+		//mStartTime = MWBase::Environment::get().getWorld()->getTimeStamp();
 		mReadyForUpdate = true;
 		mDone = false;
 		mTickCount = 0;
+		init();
 	}
 
 	void Journey::update()
 	{
-
-
-		MWWorld::Ptr npcPtr = MWBase::Environment::get().getWorld()->searchPtr(mNpcId, false);
-		MWMechanics::AiSequence& seq = npcPtr.getClass().getCreatureStats(npcPtr).getAiSequence();
+		MWWorld::Ptr npcPtr = MWBase::Environment::get().getWorld()->searchPtr(mNpcId, false); //mwx fix me, do I really need to find pointer every update? Can I cache a permanant one in life?
+		MWMechanics::AiSequence& seq = npcPtr.getClass().getCreatureStats(npcPtr).getAiSequence(); //Do I really need to find the seq ref every update? Can I cache a permanant one in life?
 		bool currentlyActive = MWBase::Environment::get().getTasksManager()->isInActiveRange(mNpcId);
 
 		if (hasArrived())
 		{
-			std::cout << "has arrived shortcut" << std::endl;
+			std::cout << mNpcId + " has arrived within radius of dest" << std::endl;
+			seq.clear();
 			mDone = true;
-			seq.clear();
 			return;
 		}
-		
-	
-		
-
-
-		if (mWasActiveLastUpdate && !currentlyActive)
+		if (mWasActiveLastUpdate && !currentlyActive) //npc has left active range since last update
 		{
-			//std::cout << "swapping active status..." << std::endl;
-			//MWX fix me sloppy function use here
-			//MWMechanics::AiSequence& seq = npcPtr.getClass().getCreatureStats(npcPtr).getAiSequence();
-			seq.clear();
-			leftActiveCells();
+			seq.clear(); //wherever npc was walking to, stop walking there.
+			mWasActiveLastUpdate = false; //flag that npc was no active this update
+			mStep -= 1; //go back one step, because now we want to teleport to the step we just tried to do
+			//leftActiveCells();
 			return;
 		}
-		else if (!currentlyActive)
+		else if (!currentlyActive) //not active, so just run inactive logic.
 		{
+			seq.clear(); //mwx fix me so darn messy this use of clear()
 			inactiveUpdate();
 			return;
 		}
 		
 		//all this below and heck probably above likely needs to be refactored mwx fix me
 
-		if (mReadyForUpdate == true || seq.getTypeId() == -1)
+		if (mReadyForUpdate == true || seq.getTypeId() == -1) //nothing in seq means AI is no longer walking, waiting for next dest.
 		{
 			if (mStep == mTravelNodeItinerary.size())
 			{
@@ -114,14 +107,12 @@ namespace MWTasks
 			{
 				mHeadedToDoor = false;
 				auto tnodeId = getBorderNodeId(mTravelNodesManager->mtravelNodeMap[mTravelNodeItinerary[mStep - 1]]->marker, mTravelNodesManager->mtravelNodeMap[mTravelNodeItinerary[mStep]]->marker);
-				
 				MWWorld::Ptr tnode = MWBase::Environment::get().getWorld()->searchPtr(tnodeId, false);
 				MWBase::Environment::get().getWorld()->activate(tnode, npcPtr);
 				std::cout << "attempted to open door" << std::endl;
-
 			}
-				
-			else {
+			else 
+			{
 				std::string tnodeId;
 				mStep += 1;
 				if (mStep == mTravelNodeItinerary.size())
@@ -161,9 +152,7 @@ namespace MWTasks
 
 	void Journey::leftActiveCells()
 	{
-
 		mWasActiveLastUpdate = false;
-
 	}
 
 	bool Journey::hasArrived()
@@ -181,23 +170,22 @@ namespace MWTasks
 
 	void Journey::inactiveUpdate()
 	{
-		if (mStep == mTravelNodeItinerary.size())
+		if (mStep == mTravelNodeItinerary.size()) //we have just teleported to the final destination
 		{
 			mDone = true;
 			return;
-
 		}
-		mTickCount += 1;
+		mTickCount += 1; //add tick, so we don't teleport every update. This can be used to determine virtual travel time. 
 		if (mTickCount >= 5)
 		{
 			std::string tnodeId;
 			mTickCount = 0;
 			mStep += 1;
-			if (mStep == mTravelNodeItinerary.size())
+			if (mStep == mTravelNodeItinerary.size()) //if we are on last step, teleport to the destination
 			{
 				tnodeId = mDestId;
 			}
-			else
+			else //otherwise navigate along bordernodes
 			{
 				//this might be better if I teleport them to the actual t node, as opposed to the transition node. Also would border node be better? Yes.
 				//tnodeId = "tn_" + std::to_string((mTravelNodeItinerary[mStep - 1])) + "to" + std::to_string(mTravelNodeItinerary[mStep]);
@@ -211,7 +199,7 @@ namespace MWTasks
 			MWWorld::Ptr marker = MWBase::Environment::get().getWorld()->searchPtr(tnodeId, false);
 			if (marker.getClass().isDoor())
 			{
-				mHeadedToDoor = true;
+				mHeadedToDoor = true; //if our destination is door, mark it as such so npc can open it if they end up in active cell
 			}
 			MWWorld::Ptr npcPtr = MWBase::Environment::get().getWorld()->searchPtr(mNpcId, false);
 			ESM::Position markerPos = marker.getRefData().getPosition();
@@ -245,23 +233,14 @@ namespace MWTasks
 		std::cout << mNpcId + "is searching for dest id: " + mDestId << std::endl;
 		MWWorld::Ptr npcPtr = MWBase::Environment::get().getWorld()->searchPtr(mNpcId, false);
 		MWWorld::Ptr destPtr = MWBase::Environment::get().getWorld()->searchPtr(mDestId, false);
-
-		
 		int currentNode = mTravelNodesManager->mCellToNodeMap[npcPtr.getCell()]->id;
 		int destNode = mTravelNodesManager->mCellToNodeMap[destPtr.getCell()]->id;
-
 		auto path = mTravelNodesManager->mtravelPathGridGraph.aStarSearch(currentNode, destNode); //WANT CURRENT NODE END NODE
-
 		for (std::list<ESM::Pathgrid::Point>::iterator it = path.begin(); it != path.end(); it++)
 				{
-					mTravelNodeItinerary.push_back(it->mUnknown);
+					mTravelNodeItinerary.push_back(it->mUnknown); //a list of all cells npc needs to travel to, 0 is current, final is where the cell that the destination object is in
 				}
-		
-		
-		
-		return true;
-
-
+		return true; //fail conditionw ill be added at some time 
 	}
 }
 
