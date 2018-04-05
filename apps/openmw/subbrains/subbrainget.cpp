a9d375c3e41e3681668e8b423f9dbf70af6d8a9b
mattwla <mattlarrabee@gmail.com>
2018-04-05 15:30:12 -0400

    starts skeleton getsubbrain

--- apps/openmw/subbrains/subbrainget.cpp
+++ apps/openmw/subbrains/subbrainget.cpp
@@ -5,12 +5,21 @@
 
 MWBase::SubBrainGet::SubBrainGet(MWBase::Life * life)
 {
+	mOwnerLife = life;
 }
 
 void MWBase::SubBrainGet::calculate(MWBase::Awareness * awareness)
 {
-
+	std::vector<SensoryLink> linkstore = awareness->getSensoryLinkStore()->mCurrentSensoryLinks;
+	std::vector<SensoryLink>::iterator it = linkstore.begin();
+	while (it != linkstore.end())
+	{
+		std::cout << "Oh I see" + it->mPtr.getCellRef().getRefId() << std::endl;
+		//for every item, create a getBO
+		it++;
+	}
 	//search through SEs that npcs can pick up, maybe even calculate cost to get each?
+	//only search throug new SEs?
 }
 
 std::string MWBase::SubBrainGet::getID()
